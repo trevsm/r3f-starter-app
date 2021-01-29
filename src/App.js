b5a0c73d9a351ec853a272e7eda69937e9d07a45
trevsm <trevsm@pm.me>
2021-01-29 15:01:50 -0700

    Simplified Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-import { Canvas } from 'react-three-fiber'
+import { Canvas, useFrame } from 'react-three-fiber'
 import { OrbitControls, softShadows } from '@react-three/drei'
-
-import { Lighting } from './lighting/Lighting'
-import { Terrain } from './terrain/Terrain'
-import { Box } from './object/Box.js'
-import { Stats } from './stats/Stats'
 
 import './App.css'
 
 softShadows()
 
+function Box(){
+  const mesh = useRef()
+
+  useFrame(()=>{
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.002
+  })
+  
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )
+}
+
 export default function App() {
   return (
     <Canvas
-      shadowMap
-      gl={{ alpha: false }}
       camera={{ position: [-5, 2, 10], fov: 60 }}
     >
-      <Stats />
-      <OrbitControls />
-      <Lighting />
-      <Box />
-      <Terrain />
+      <OrbitControls/>
+      <Box/>
     </Canvas>
   )
 }
